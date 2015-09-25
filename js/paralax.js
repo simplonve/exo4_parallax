86455701dd15bb907af8f93244b4c3568582af85
mathf07 <mathf07@laposte.net>
2015-09-25 14:45:26 +0200

    solution translate jquery parallax into javascript

--- js/paralax.js
+++ js/paralax.js
@@ -1,10 +1,40 @@
-$(document).ready(function(){
-  $('div[data-type="background"]').each(function(){
-    var $background_object = $(this);
-    $(window).scroll(function() {
-      var yPos = -($(window).scrollTop() / $background_object.data('speed'));
-      var coords = '50% '+ yPos + 'px';
-      $background_object.css({ backgroundPosition: coords });
-    });
-  });
-});
+function parallax () {
+	var background = document.querySelectorAll('div[data-type="background"]');
+	for (var image = 0; image < background.length; image++) {
+		var ypos = (-window.pageYOffset / background[image].getAttribute('data-speed'));
+		background[image].style.backgroundPosition = '50% '+ ypos +'px';
+		// var coords = '50% '+ ypos +'px';
+	}
+}
+window.onload = (function (){
+  window.addEventListener('scroll', parallax);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   $('div[data-type="background"]').each(function(){
+//     var $background_object = $(this);
+//     $(window).scroll(function() {
+//       var yPos = -($(window).scrollTop() / $background_object.data('speed'));
+//       var coords = '50% '+ yPos + 'px';
+//       $background_object.css({ backgroundPosition: coords });
+//     });
+//   });
+// });
