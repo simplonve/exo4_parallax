b9b3148f6736faa6588fa7c5ddf6821e6f0264c4
p-guerin <guerin.pierre26@gmail.com>
2015-09-25 14:43:51 +0200

    Ajout de la solution de remi et pierre

--- js/paralax.js
+++ js/paralax.js
@@ -1,10 +1,20 @@
-$(document).ready(function(){
-  $('div[data-type="background"]').each(function(){
-    var $background_object = $(this);
-    $(window).scroll(function() {
-      var yPos = -($(window).scrollTop() / $background_object.data('speed'));
+window.onload = (function(){
+  [].forEach.call(document.querySelectorAll('div[data-type="background"]'), function(item, i){
+    window.addEventListener("scroll", function(){
+      var yPos = -(window.pageYOffset / item.getAttribute("data-speed"));
       var coords = '50% '+ yPos + 'px';
-      $background_object.css({ backgroundPosition: coords });
+      item.style.backgroundPosition = coords;
     });
   });
 });
+
+// $(document).ready(function(){
+//   $('div[data-type="background"]').each(function(){
+//     var $background_object = $(this);
+//     $(window).scroll(function() {
+//       var yPos = -($(window).scrollTop() / $background_object.data('speed'));
+//       var coords = '50% '+ yPos + 'px';
+//       $background_object.css({ backgroundPosition: coords });
+//     });
+//   });
+// });
